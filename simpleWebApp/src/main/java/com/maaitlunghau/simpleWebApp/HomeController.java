ab0540e028b30fb2ad6d092c6c8b43a31055fcfd
maaitlunghau <chunhau.py@gmail.com>
2026-07-09 22:12:06 +0700

    feat(simplewebapp): add HomeController and LoginController with basic routes

---
+++ simpleWebApp/src/main/java/com/maaitlunghau/simpleWebApp/HomeController.java
@@ -0,0 +1,20 @@
+package com.maaitlunghau.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody // maybe don't need this
+    public String greet() {
+        return "Welcome to my simple web application!";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "This is a simple web application built with Spring Boot.";
+    }
+}
