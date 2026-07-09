efc9a4f875e9170d24714c75e6cef227e75c24b5
maaitlunghau <chunhau.py@gmail.com>
2026-07-09 22:38:45 +0700

    refactor(simplewebapp): restructure folder project

--- simpleWebApp/src/main/java/com/maaitlunghau/simpleWebApp/controller/LoginController.java
+++ simpleWebApp/src/main/java/com/maaitlunghau/simpleWebApp/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.maaitlunghau.simpleWebApp;
+package com.maaitlunghau.simpleWebApp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
