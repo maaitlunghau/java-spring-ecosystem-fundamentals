9d9e807f7306890e1d2a9261c5e11e5192b265e1
maaitlunghau <chunhau.py@gmail.com>
2026-07-10 13:08:28 +0700

    chore: initialize new spring boot project - rest api jpa mysql

---
+++ projects/04-rest-api-jpa-mysql/src/main/java/com/maaitlunghau/__rest_api_jpa_mysql/Application.java
@@ -0,0 +1,13 @@
+package com.maaitlunghau.__rest_api_jpa_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
