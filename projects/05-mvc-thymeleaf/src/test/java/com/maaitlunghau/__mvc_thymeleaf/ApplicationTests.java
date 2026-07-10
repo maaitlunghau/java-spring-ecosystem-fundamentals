41d0dcdd49ffbb398175e497608edcc48203deeb
maaitlunghau <chunhau.py@gmail.com>
2026-07-10 14:30:13 +0700

    chore: initialize new spring boot project - myc thymeleaf

---
+++ projects/05-mvc-thymeleaf/src/test/java/com/maaitlunghau/__mvc_thymeleaf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaitlunghau.__mvc_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
