cc0199d40d743813934dcea2446fee4f499d364e
maaitlunghau <chunhau.py@gmail.com>
2026-07-11 18:07:15 +0700

    feat(auth0-mvc): add home controller

---
+++ projects/08-spring-security-auth0-mvc/src/main/java/com/maaitlunghau/__spring_security_auth0_mvc/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.maaitlunghau.__spring_security_auth0_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+}
