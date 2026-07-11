a63de8c87b2bec9831371fefd626249e0e31b821
maaitlunghau <chunhau.py@gmail.com>
2026-07-11 09:51:03 +0700

    feat: add AuthenticationController for register and login endpoints

---
+++ projects/06-spring-security-jwt/src/main/java/com/maaitlunghau/__spring_security_jwt/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.maaitlunghau.__spring_security_jwt.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maaitlunghau.__spring_security_jwt.model.AuthenticationResponse;
+import com.maaitlunghau.__spring_security_jwt.model.User;
+import com.maaitlunghau.__spring_security_jwt.service.AuthenticationService;
+
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody User request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody User request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}   
