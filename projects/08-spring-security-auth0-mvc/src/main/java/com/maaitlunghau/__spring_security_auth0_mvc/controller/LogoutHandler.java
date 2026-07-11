d8acfe6eb1e9c0b1dddbbb9fff44dd9383e92f14
maaitlunghau <chunhau.py@gmail.com>
2026-07-11 18:58:15 +0700

    fix(auth0-mvc): change LogoutHandler annotation from @Controller to @Component

--- projects/08-spring-security-auth0-mvc/src/main/java/com/maaitlunghau/__spring_security_auth0_mvc/controller/LogoutHandler.java
+++ projects/08-spring-security-auth0-mvc/src/main/java/com/maaitlunghau/__spring_security_auth0_mvc/controller/LogoutHandler.java
@@ -3,14 +3,14 @@ package com.maaitlunghau.__spring_security_auth0_mvc.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Controller
+@Component
 public class LogoutHandler extends SecurityContextLogoutHandler {
 
     private final ClientRegistrationRepository clientRegistrationRepository;
