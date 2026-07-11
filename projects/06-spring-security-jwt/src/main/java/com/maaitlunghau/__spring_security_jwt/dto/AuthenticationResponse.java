f9afd4f34fd4f14316a28bb71c90d64a93951562
maaitlunghau <chunhau.py@gmail.com>
2026-07-11 10:58:18 +0700

    feat: add refreshToken field to AuthenticationResponse

--- projects/06-spring-security-jwt/src/main/java/com/maaitlunghau/__spring_security_jwt/dto/AuthenticationResponse.java
+++ projects/06-spring-security-jwt/src/main/java/com/maaitlunghau/__spring_security_jwt/dto/AuthenticationResponse.java
@@ -1,3 +1,3 @@
 package com.maaitlunghau.__spring_security_jwt.dto;
 
-public record AuthenticationResponse(String token) {}
+public record AuthenticationResponse(String accessToken, String refreshToken) {}
