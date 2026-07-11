03d174fab4e8d9992e70f8b4861eddb30491b8e8
maaitlunghau <chunhau.py@gmail.com>
2026-07-11 10:17:49 +0700

    feat: add HomeController with a secure home endpoint

---
+++ projects/06-spring-security-jwt/src/main/java/com/maaitlunghau/__spring_security_jwt/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.maaitlunghau.__spring_security_jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello dev from secure URL";
+    }
+}
