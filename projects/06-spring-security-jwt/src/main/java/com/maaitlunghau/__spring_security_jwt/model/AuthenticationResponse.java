5d978e8a1648e63f4a4261a658352568cb6c898a
maaitlunghau <chunhau.py@gmail.com>
2026-07-11 09:51:00 +0700

    feat: add AuthenticationService with register and login logic

---
+++ projects/06-spring-security-jwt/src/main/java/com/maaitlunghau/__spring_security_jwt/model/AuthenticationResponse.java
@@ -0,0 +1,3 @@
+package com.maaitlunghau.__spring_security_jwt.model;
+
+public record AuthenticationResponse(String token) {}
---
+++ projects/06-spring-security-jwt/src/main/java/com/maaitlunghau/__spring_security_jwt/service/AuthenticationService.java
@@ -0,0 +1,58 @@
+package com.maaitlunghau.__spring_security_jwt.service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.maaitlunghau.__spring_security_jwt.model.AuthenticationResponse;
+import com.maaitlunghau.__spring_security_jwt.model.User;
+import com.maaitlunghau.__spring_security_jwt.repository.UserRepository;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+        UserRepository userRepository, 
+        PasswordEncoder passwordEncoder,
+        JwtService jwtService,
+        AuthenticationManager authenticationManager
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthenticationResponse register(User request) {
+        User user = new User(
+            request.getFirstName(),
+            request.getLastName(),
+            request.getUsername(),
+            passwordEncoder.encode(request.getPassword()),
+            request.getRole()
+        );
+
+        user = userRepository.save(user);
+
+        String token = jwtService.generateToken(user);
+
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(User request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        ); 
+
+        User user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.generateToken(user);
+
+        return new AuthenticationResponse(token);
+    }
+}
