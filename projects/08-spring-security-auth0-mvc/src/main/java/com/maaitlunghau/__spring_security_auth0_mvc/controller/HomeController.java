f086b58cb72e618f6f239a03026aa2f8aac68faa
maaitlunghau <chunhau.py@gmail.com>
2026-07-11 18:36:48 +0700

    feat(auth0-mvc): display oidc user profile with avatar and name

--- projects/08-spring-security-auth0-mvc/src/main/java/com/maaitlunghau/__spring_security_auth0_mvc/controller/HomeController.java
+++ projects/08-spring-security-auth0-mvc/src/main/java/com/maaitlunghau/__spring_security_auth0_mvc/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.maaitlunghau.__spring_security_auth0_mvc.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +15,8 @@ public class HomeController {
     }
 
     @GetMapping("/profile")
-    public String profile() {
+    public String profile(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
+        model.addAttribute("profile", oidcUser.getClaims());
         return "profile";
     }
 }
